1ce244c46cd5813ea27bb14e198b5976cfee5f04
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-17 14:00:39 +0800

    update

--- overlays/catppuccin-cursors.nix
+++ overlays/catppuccin-cursors.nix
@@ -2,13 +2,15 @@
 
 stdenv.mkDerivation rec {
   pname = "cattpuccin-cursors";
-  version = "0.2.7";
+  version = "9999";
 
   src = fetchzip {
-    url =
-      "https://github.com/catppuccin/cursors/raw/21942800ad34b357a12079718a1faa88f0bccf28/cursors/Catppuccin-Frappe-Dark-Cursors.zip";
-    sha256 = "RCEVxeo3obNqHogxWMfPoQotirSQTMw15zCahWto=";
-  };
+      url =
+        "https://github.com/Ruixi-rebirth/Catppuccin-cursor/archive/refs/tags/1.0.zip";
+      sha256 = "w7yv9e9MuZgmCdr/RdDxg2hAeIhb1f82idUj4diI8v8=";
+      stripRoot = false;
+    };
+
 
   installPhase = ''
     mkdir -p $out/share/icons/Catppuccin-Frappe-Dark
@@ -17,7 +19,7 @@ stdenv.mkDerivation rec {
 
   meta = {
     description = "Soothing pastel mouse cursors";
-    homepage = "https://github.com/catppuccin/cursors";
+    homepage = "https://github.com/Ruixi-rebirth/Catppuccin-cursor";
     license = lib.licenses.gpl3;
     platforms = lib.platforms.unix;
     maintainers = [ lib.maintainers.Ruixi-rebirth ];
