113f5b59dfd6fa6fc168c39b7a56f063c851ecab
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-17 21:00:36 +0800

    update

---
+++ modules/programs/misc_waybar.nix
@@ -0,0 +1,13 @@
+
+let
+  Cava = pkgs.writeShellScriptBin "Cava" ''
+    #!/bin/bash
+    killall cava
+    cava -p ~/.config/cava/config1 | sed -u 's/;//g;s/0/▁/g;s/1/▂/g;s/2/▃/g;s/3/▄/g;s/4/▅/g;s/5/▆/g;s/6/▇/g;s/7/█/g;'
+  '';
+   wallpaper_random= pkgs.writeShellScriptBin "wallpaper_random" ''
+    #!/bin/bash
+    killall swaybg
+    swaybg -i $(find ~/Pictures/wallpaper/. -name "*.png" | shuf -n1) -m fill &
+  '';
+in
