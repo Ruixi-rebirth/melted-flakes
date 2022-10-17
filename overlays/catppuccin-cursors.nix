7540cd890091ee87c9b36bb465fa3ce0aa67b1a4
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-17 15:41:57 +0800

    update

--- overlays/catppuccin-cursors.nix
+++ overlays/catppuccin-cursors.nix
@@ -5,6 +5,7 @@
     src = fetchzip {
         url =
           "https://github.com/Ruixi-rebirth/Catppuccin-cursor/archive/refs/tags/1.0.zip";
+          sha256="sha256-RCEVxeo3oBNqHogxWM/YqfPoQotirSQTMw15zCahWto=";
       };
     installPhase = ''
       mkdir -p $out/share/icons/Catppuccin-Frappe-Dark
