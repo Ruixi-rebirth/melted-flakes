6e07e79e4674a18354c47cb9877d1be8bed7ccc4
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-17 14:11:23 +0800

    update

--- overlays/catppuccin-gtk.nix
+++ overlays/catppuccin-gtk.nix
@@ -1,30 +1,5 @@
 { lib, stdenv, fetchzip, pkgs, ... }:
 
-stdenv.mkDerivation rec {
-  pname = "cattpuccin-cursors";
-  version = "9999";
-
-  src = fetchzip {
-      url =
-        "https://github.com/Ruixi-rebirth/Catppuccin-cursor/archive/refs/tags/1.0.zip";
-      sha256 = "w7yv9e9MuZgmCdr/RdDxg2hAeIhb1f82idUj4diI8v8=";
-      stripRoot = false;
-    };
-
-
-  installPhase = ''
-    mkdir -p $out/share/icons/Catppuccin-Frappe-Dark
-    cp -va index.theme cursors $out/share/icons/Catppuccin-Frappe-Dark
-  '';
-
-  meta = {
-    description = "Soothing pastel mouse cursors";
-    homepage = "https://github.com/Ruixi-rebirth/Catppuccin-cursor";
-    license = lib.licenses.gpl3;
-    platforms = lib.platforms.unix;
-    maintainers = [ lib.maintainers.Ruixi-rebirth ];
-  };
-}
 
 stdenv.mkDerivation rec {
   pname = "cattpuccin-gtk";
@@ -54,4 +29,28 @@ stdenv.mkDerivation rec {
     platforms = lib.platforms.unix;
     maintainers = [ lib.maintainers.Ruixi-rebirth ];
   };
+    pname = "cattpuccin-cursors";
+  version = "9999";
+
+  src = fetchzip {
+      url =
+        "https://github.com/Ruixi-rebirth/Catppuccin-cursor/archive/refs/tags/1.0.zip";
+      sha256 = "w7yv9e9MuZgmCdr/RdDxg2hAeIhb1f82idUj4diI8v8=";
+      stripRoot = false;
+    };
+
+
+  installPhase = ''
+    mkdir -p $out/share/icons/Catppuccin-Frappe-Dark
+    cp -va index.theme cursors $out/share/icons/Catppuccin-Frappe-Dark
+  '';
+
+  meta = {
+    description = "Soothing pastel mouse cursors";
+    homepage = "https://github.com/Ruixi-rebirth/Catppuccin-cursor";
+    license = lib.licenses.gpl3;
+    platforms = lib.platforms.unix;
+    maintainers = [ lib.maintainers.Ruixi-rebirth ];
+  };
+
 }
