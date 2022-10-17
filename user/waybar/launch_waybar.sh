f363c995bf0caf78ee23b68084640b9c2e11c819
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-17 19:55:09 +0800

    update

--- user/waybar/launch_waybar.sh
+++ user/waybar/launch_waybar.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 killall waybar
 
+SDIR="$HOME/.config/waybar"
+
 for i in /sys/class/hwmon/hwmon*/temp*_input; do 
     if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "coretemp: Core 0" ]; then
         export HWMON_PATH="$i"
