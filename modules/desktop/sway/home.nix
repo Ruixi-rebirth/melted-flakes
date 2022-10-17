40f9beefd678163c6149460df373abbca18f5c8a
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-17 21:36:28 +0800

    update

--- modules/desktop/sway/home.nix
+++ modules/desktop/sway/home.nix
@@ -45,7 +45,13 @@ let
   '';
   dynamic_wallpaper = pkgs.writeShellScriptBin "dynamic_wallpaper" ''
     #!/bin/bash
-    killall swaybg
+    is_swaybg_ServerExist=`ps -ef|grep -m 1 swaybg|grep -v "grep"|wc -l`
+    if [ "$is_swaybg_ServerExist" = "0" ]; then
+      echo "swaybg_server not found" > /dev/null 2>&1
+#	exit;
+    elif [ "$is_swaybg_ServerExist" = "1" ]; then
+      killall swaybg
+    fi
 # Automatically change wallpaper every 10 minutes
     swaybg -i $(find ~/Pictures/wallpaper/. -name "*.png" | shuf -n1) -m fill &
     OLD_PID=$!
--- modules/scripts/home.nix
+++ modules/scripts/home.nix
@@ -3,12 +3,17 @@
 let
   Cava = pkgs.writeShellScriptBin "Cava" ''
     #!/bin/bash
-    killall cava
     cava -p ~/.config/cava/config1 | sed -u 's/;//g;s/0/▁/g;s/1/▂/g;s/2/▃/g;s/3/▄/g;s/4/▅/g;s/5/▆/g;s/6/▇/g;s/7/█/g;'
   '';
    wallpaper_random= pkgs.writeShellScriptBin "wallpaper_random" ''
     #!/bin/bash
-    killall swaybg
+    is_swaybg_ServerExist=`ps -ef|grep -m 1 swaybg|grep -v "grep"|wc -l`
+    if [ "$is_swaybg_ServerExist" = "0" ]; then
+      echo "swaybg_server not found" > /dev/null 2>&1
+#	exit;
+    elif [ "$is_swaybg_ServerExist" = "1" ]; then
+      killall swaybg
+    fi
     swaybg -i $(find ~/Pictures/wallpaper/. -name "*.png" | shuf -n1) -m fill &
   '';
 in 
