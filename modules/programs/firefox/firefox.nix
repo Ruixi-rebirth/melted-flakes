04bb36686c2a380754a89701814e34c6eb62dbaa
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-17 12:57:04 +0800

    update

--- modules/programs/firefox/firefox.nix
+++ modules/programs/firefox/firefox.nix
@@ -5,14 +5,6 @@
     enable = true;
     package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
       forceWayland = true;
-      ExtensionSettings = [
-        (fetchFirefoxAddon {
-          name = "catppuccin-frappe-pink";
-        url =
-          "https://github.com/catppuccin/firefox/releases/download/old/catppuccin_frappe_pink.xpi";
-          sha256 = "UMkjWqNUzk72ZlP1roh1e4xlUpfDYrkKidRTIfAem9M=";
-        })
-      ];
       extraPolicies = {
         DisplayBookmarksToolbar = false;    
         Preferences = {
