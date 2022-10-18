662655cd245887e9f7abd087eb8c7ac308d2de0d
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-18 14:42:21 +0800

    update

--- modules/programs/net-im.nix
+++ modules/programs/net-im.nix
@@ -1,4 +1,7 @@
 { config, pkgs, ... }:
+let
+  nur-no-pkgs = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {};
+in
 {
    home = {                                
      packages = with pkgs; [
