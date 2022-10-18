c021ddb9ff09157224d99eee117d0665df1dac37
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-18 08:32:53 +0800

    update

--- modules/scripts/home.nix
+++ modules/scripts/home.nix
@@ -2,7 +2,7 @@
 
 let
   Cava = pkgs.writeShellScriptBin "Cava" ''
-    #!/bin/bash
+    #!/bin/sh
     cava -p ~/.config/cava/config1 | sed -u 's/;//g;s/0/▁/g;s/1/▂/g;s/2/▃/g;s/3/▄/g;s/4/▅/g;s/5/▆/g;s/6/▇/g;s/7/█/g;'
   '';
    wallpaper_random= pkgs.writeShellScriptBin "wallpaper_random" ''
