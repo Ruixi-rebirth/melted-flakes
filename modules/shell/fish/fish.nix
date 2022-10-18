8fe538536866cf22ea0b29d4fbce83351391c736
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-19 00:10:07 +0800

    update

--- modules/shell/fish/fish.nix
+++ modules/shell/fish/fish.nix
@@ -2,7 +2,7 @@
 
 {
    home.file.".config/fish/conf.d/mocha.fish".text = import ./conf.d/mocha_theme.nix;
-   # home.file.".config/fish/config.fish".text = import ./config.nix;
+   home.file.".config/fish/config.fish".text = import ./config.nix;
    # home.file.".config/fish/fish_variables".text = import ./fish_variables.nix;
    home.file.".config/fish/functions".source = ./functions;
 }
