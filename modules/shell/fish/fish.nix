4abfc9637fc98deacfe0f7e9ba56c875dc01e2a5
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-18 21:22:25 +0800

    update

--- modules/shell/fish/fish.nix
+++ modules/shell/fish/fish.nix
@@ -10,9 +10,21 @@
       
          set -g fish_greeting
        '';
+      functions = {
+        
+      };
+      shellAliases = {
+        nvim = "vi";
+        top = "btop";
+        l = "ls -ahl";
+        ls = "lsd";
+        mpv = "env __NV_PRIME_RENDER_OFFLOAD=1 mpv";
+        n = "neofeth";
+        r = "ranger";
+      };
     };
   };
-   home.file.".config/fish/fish_variables".source = ./fish_variables;
+   # home.file.".config/fish/fish_variables".source = ./fish_variables;
    home.file.".config/fish/conf.d/mocha.fish".source = ./conf.d/mocha.fish;
-   home.file.".config/fish/functions".source = ./functions;
+   # home.file.".config/fish/functions".source = ./functions;
 }
