b2096455627339fee523b901a981c10bfc54014a
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-18 21:44:26 +0800

    update

--- modules/shell/fish/fish.nix
+++ modules/shell/fish/fish.nix
@@ -26,5 +26,5 @@
    home.file.".config/fish/functions/fish_prompt.fish".source = ./functions/fish_prompt.fish;
    home.file.".config/fish/functions/owf.fish".source = ./functions/owf.fish;
    home.file.".config/fish/functions/f.fish".source = ./functions/f.fish;
-   home.file.".config/fish/fish_variables".text = import ./fish_variables.nix;
+  # home.file.".config/fish/fish_variables".text = import ./fish_variables.nix;
 }
