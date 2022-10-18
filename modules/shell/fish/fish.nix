2e29d612a7fc292a61582a58e9f0bca3c5f222d6
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-18 22:05:16 +0800

    update

--- modules/shell/fish/fish.nix
+++ modules/shell/fish/fish.nix
@@ -8,20 +8,20 @@
          set TTY1 (tty)
          [ "$TTY1" = "/dev/tty1" ] && exec dbus-run-session sway --unsupported-gpu
        '';
-      shellAliases = {
-        nvim = "vi";
-        top = "btop";
-        l = "ls -ahl";
-        ls = "lsd";
-        mpv = "env __NV_PRIME_RENDER_OFFLOAD=1 mpv";
-        n = "neofeth";
-        r = "ranger";
-      };
+  #    shellAliases = {
+   #     nvim = "vi";
+    #    top = "btop";
+     #   l = "ls -ahl";
+      #  ls = "lsd";
+       # mpv = "env __NV_PRIME_RENDER_OFFLOAD=1 mpv";
+       # n = "neofeth";
+       # r = "ranger";
+     # };
     };
   };
- #  home.file.".config/fish/conf.d/mocha.fish".source = ./conf.d/mocha.fish;
- #  home.file.".config/fish/functions/fish_prompt.fish".source = ./functions/fish_prompt.fish;
- #  home.file.".config/fish/functions/owf.fish".source = ./functions/owf.fish;
- #  home.file.".config/fish/functions/f.fish".source = ./functions/f.fish;
-  # home.file.".config/fish/fish_variables".text = import ./fish_variables.nix;
+   home.file.".config/fish/conf.d/mocha.fish".source = ./conf.d/mocha.fish;
+   home.file.".config/fish/functions/fish_prompt.fish".source = ./functions/fish_prompt.fish;
+   home.file.".config/fish/functions/owf.fish".source = ./functions/owf.fish;
+   home.file.".config/fish/functions/f.fish".source = ./functions/f.fish;
+   home.file.".config/fish/fish_variables".text = import ./fish_variables.nix;
 }
