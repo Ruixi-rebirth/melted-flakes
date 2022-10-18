2cbb49aa5337eb2235f03bfdfcd52116fc5d09a2
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-19 01:21:31 +0800

    update

--- modules/shell/fish/fish.nix
+++ modules/shell/fish/fish.nix
@@ -1,6 +1,7 @@
 { lib, pkgs, user,  ... }:
 
 {
+   programs.fish.enable = true;
    home.file.".config/fish/conf.d/mocha.fish".text = import ./conf.d/mocha_theme.nix;
    home.file.".config/fish/config.fish".text = import ./config.nix;
    home.file.".config/fish/fish_variables".text = import ./fish_variables.nix;
