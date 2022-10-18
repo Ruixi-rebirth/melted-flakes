4b32ed0bbcb20f581af300dcfbf9444378b83746
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-18 16:24:55 +0800

    update

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -7,6 +7,7 @@
     (import ../../modules/shell) ++
     (import ../../modules/editors) ++
     (import ../../modules/programs) ++
+    [(import ../../modules/environment/variables.nix)] ++
     (import ../../modules/theme) ++
     (import ../../modules/dev_lang);
 
