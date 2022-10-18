88794f480419f932a5931ac77a90481f4a740b95
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-18 22:01:43 +0800

    update

--- modules/shell/fish/fish.nix
+++ modules/shell/fish/fish.nix
@@ -8,9 +8,6 @@
          set TTY1 (tty)
          [ "$TTY1" = "/dev/tty1" ] && exec dbus-run-session sway --unsupported-gpu
        '';
-      functions = {
-        
-      };
       shellAliases = {
         nvim = "vi";
         top = "btop";
