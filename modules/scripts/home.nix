dcc7dc29b2025c65661b9d3c84014191899a59ba
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-18 08:06:08 +0800

    update

--- modules/scripts/home.nix
+++ modules/scripts/home.nix
@@ -16,10 +16,76 @@ let
     fi
     swaybg -i $(find ~/Pictures/wallpaper/. -name "*.png" | shuf -n1) -m fill &
   '';
+  grimshot_watermark = pkgs.writeShellScriptBin "grimshot_watermark" ''
+    #!/bin/bash
+    FILE=$(date "+%Y-%m-%d"T"%H:%M:%S").png
+# Get the picture from maim
+    grimshot --notify  save area ~/Pictures/src.png >> /dev/null 2>&1
+# add shadow, round corner, border and watermark
+    convert $HOME/Pictures/src.png \
+      \( +clone -alpha extract \
+      -draw 'fill black polygon 0,0 0,8 8,0 fill white circle 8,8 8,0' \
+      \( +clone -flip \) -compose Multiply -composite \
+      \( +clone -flop \) -compose Multiply -composite \
+      \) -alpha off -compose CopyOpacity -composite $HOME/Pictures/output.png
+#
+    convert $HOME/Pictures/output.png -bordercolor none -border 20 \( +clone -background black -shadow 80x8+15+15 \) \
+      +swap -background transparent -layers merge +repage $HOME/Pictures/$FILE
+#
+    composite -gravity Southeast "${./watermark.png}" $HOME/Pictures/$FILE $HOME/Pictures/$FILE 
+#
+# # Send the Picture to clipboard
+    wl-copy < $HOME/Pictures/$FILE
+#
+# # remove the other pictures
+    rm $HOME/Pictures/src.png $HOME/Pictures/output.png
+  '';
+  myswaylock = pkgs.writeShellScriptBin "myswaylock" ''
+    #!/bin/bash
+    swaylock  \
+           --screenshots \
+           --clock \
+           --indicator \
+           --indicator-radius 100 \
+           --indicator-thickness 7 \
+           --effect-blur 7x5 \
+           --effect-vignette 0.5:0.5 \
+           --ring-color 3b4252 \
+           --key-hl-color 880033 \
+           --line-color 00000000 \
+           --inside-color 00000088 \
+           --separator-color 00000000 \
+           --grace 2 \
+           --fade-in 0.3
+  '';
+  dynamic_wallpaper = pkgs.writeShellScriptBin "dynamic_wallpaper" ''
+    #!/bin/bash
+    is_swaybg_ServerExist=`ps -ef|grep -m 1 swaybg|grep -v "grep"|wc -l`
+    if [ "$is_swaybg_ServerExist" = "0" ]; then
+      echo "swaybg_server not found" > /dev/null 2>&1
+#	exit;
+    elif [ "$is_swaybg_ServerExist" = "1" ]; then
+      killall swaybg
+    fi
+# Automatically change wallpaper every 10 minutes
+    swaybg -i $(find ~/Pictures/wallpaper/. -name "*.png" | shuf -n1) -m fill &
+    OLD_PID=$!
+    while true; do
+        sleep 120
+        swaybg -i $(find ~/Pictures/wallpaper/. -name "*.png" | shuf -n1) -m fill &
+        NEXT_PID=$!
+        sleep 5
+        kill $OLD_PID
+        OLD_PID=$NEXT_PID
+    done
+  '';
 in 
 {
    home.packages = with pkgs; [
-     Cava
-     wallpaper_random
+      Cava
+      wallpaper_random
+      grimshot_watermark
+      myswaylock
+      dynamic_wallpaper
     ];
 }
