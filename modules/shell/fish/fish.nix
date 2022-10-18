211be5f617722e0890ba9fdf56525b65e5cc1883
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-18 23:35:06 +0800

    update

--- modules/shell/fish/fish.nix
+++ modules/shell/fish/fish.nix
@@ -1,22 +1,9 @@
 { lib, pkgs, user,  ... }:
 
 {
-  programs = {
-    fish = {
-      shellAliases = {
-       nvim = "vi";
-       top = "btop";
-        l = "ls -ahl";
-        ls = "lsd";
-        mpv = "env __NV_PRIME_RENDER_OFFLOAD=1 mpv";
-        n = "neofeth";
-        r = "ranger";
-      };
-    };
-  };
- #  home.file.".config/fish/conf.d/mocha.fish".source = ./conf.d/mocha.fish;
- #  home.file.".config/fish/functions/fish_prompt.fish".source = ./functions/fish_prompt.fish;
- #  home.file.".config/fish/functions/owf.fish".source = ./functions/owf.fish;
- #  home.file.".config/fish/functions/f.fish".source = ./functions/f.fish;
- #  home.file.".config/fish/fish_variables".text = import ./fish_variables.nix;
+   home.file.".config/fish/conf.d/mocha.fish".source = ./conf.d/mocha.fish;
+   home.file.".config/fish/functions/fish_prompt.fish".source = ./functions/fish_prompt.fish;
+   home.file.".config/fish/functions/owf.fish".source = ./functions/owf.fish;
+   home.file.".config/fish/functions/f.fish".source = ./functions/f.fish;
+   home.file.".config/fish/fish_variables".text = import ./fish_variables.nix;
 }
