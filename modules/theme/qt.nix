22e5132b1fab8fb8615e2424183b744b80fca4e5
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-18 15:44:14 +0800

    update

--- modules/theme/qt.nix
+++ modules/theme/qt.nix
@@ -4,11 +4,12 @@
   home = {                                
     packages = with pkgs; [
       pkgs.libsForQt5.qtstyleplugins
+      qt5ct
     ];
   };
   qt={
     enable = true;
     platformTheme = "gtk";
-    # style.name = "gtk2";
+    style.name = "gtk2";
   };
 }
