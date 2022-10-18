4f314461207c018269edfc8c4667dff9ff22854b
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-18 13:20:56 +0800

    update

--- modules/hardware/hotspot.nix
+++ modules/hardware/hotspot.nix
@@ -6,7 +6,7 @@
   services.create_ap = {
   enable = true;
   settings = {
-    INTERNET_IFACE = "enp65s0";
+    INTERNET_IFACE = "wlp0s20f3";
     WIFI_IFACE = "wlp0s20f3";
     SSID = "NixOS";
     PASSPHRASE = "12345678";
