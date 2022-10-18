be904846dd577d33627048f61a77dde329efc907
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-18 22:09:29 +0800

    update

--- modules/shell/fish/fish.nix
+++ modules/shell/fish/fish.nix
@@ -4,19 +4,16 @@
   programs = {
     fish = {
       enable = true;
-       loginShellInit = ''
-         set TTY1 (tty)
-         [ "$TTY1" = "/dev/tty1" ] && exec dbus-run-session sway --unsupported-gpu
-       '';
-  #    shellAliases = {
-   #     nvim = "vi";
-    #    top = "btop";
-     #   l = "ls -ahl";
-      #  ls = "lsd";
-       # mpv = "env __NV_PRIME_RENDER_OFFLOAD=1 mpv";
-       # n = "neofeth";
-       # r = "ranger";
-     # };
+      
+      shellAliases = {
+       nvim = "vi";
+       top = "btop";
+        l = "ls -ahl";
+        ls = "lsd";
+        mpv = "env __NV_PRIME_RENDER_OFFLOAD=1 mpv";
+        n = "neofeth";
+        r = "ranger";
+      };
     };
   };
    home.file.".config/fish/conf.d/mocha.fish".source = ./conf.d/mocha.fish;
