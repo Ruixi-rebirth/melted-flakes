f86b1f589645e0deb64a1983e751c679f7746934
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-18 18:07:54 +0800

    update

--- modules/shell/fish.nix
+++ modules/shell/fish.nix
@@ -3,6 +3,10 @@
   programs = {
     fish = {
       enable = true;
+      loginShellInit = ''
+        set TTY1 (tty)
+        [ "$TTY1" = "/dev/tty1" ] && exec dbus-run-session sway --unsupported-gpu
+      '';
     };
   };
 }
