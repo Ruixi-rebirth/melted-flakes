37158ad8a24c6ea75c026a0d16a6ebb5bdc35733
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-18 22:16:57 +0800

    update

--- modules/shell/fish/fish.nix
+++ modules/shell/fish/fish.nix
@@ -16,9 +16,9 @@
     #  };
     };
   };
-   home.file.".config/fish/conf.d/mocha.fish".source = ./conf.d/mocha.fish;
-   home.file.".config/fish/functions/fish_prompt.fish".source = ./functions/fish_prompt.fish;
-   home.file.".config/fish/functions/owf.fish".source = ./functions/owf.fish;
-   home.file.".config/fish/functions/f.fish".source = ./functions/f.fish;
-   home.file.".config/fish/fish_variables".text = import ./fish_variables.nix;
+ #  home.file.".config/fish/conf.d/mocha.fish".source = ./conf.d/mocha.fish;
+ #  home.file.".config/fish/functions/fish_prompt.fish".source = ./functions/fish_prompt.fish;
+ #  home.file.".config/fish/functions/owf.fish".source = ./functions/owf.fish;
+ #  home.file.".config/fish/functions/f.fish".source = ./functions/f.fish;
+ #  home.file.".config/fish/fish_variables".text = import ./fish_variables.nix;
 }
