ed1b4d6c8010755450b1f4318b493402e93093ff
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-19 22:16:26 +0800

    update

--- hosts/system.nix
+++ hosts/system.nix
@@ -35,7 +35,7 @@ let
       in ''
         export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
         gnome_schema=org.gnome.desktop.interface
-        gsettings set $gnome_schema gtk-theme 'Dracula'
+        gsettings set $gnome_schema gtk-theme 'Catppuccin-Frappe-Pink'
         '';
   };
 in
