e2cea2304f546fa61129b35faf66f7e029b7aade
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-19 14:18:43 +0800

    update

--- modules/programs/firefox/firefox.nix
+++ modules/programs/firefox/firefox.nix
@@ -3,7 +3,7 @@
 {
   programs.firefox = {
     enable = true;
-    package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
+    package = pkgs.wrapFirefox pkgs.firefox-esr-102-unwrapped {
       forceWayland = true;
       nixExtensions = [
           (pkgs.fetchFirefoxAddon{
