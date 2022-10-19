f33a4732dea2a79c56018bd6eec1f3282587fc71
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-10-19 14:04:17 +0800

    update

--- modules/programs/firefox/firefox.nix
+++ modules/programs/firefox/firefox.nix
@@ -10,7 +10,91 @@
         Preferences = {
           "browser.toolbars.bookmarks.visibility" = "never";
           "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          "media.ffmpeg.vaapi.enabled" = true;
+          "browser.startup.homepage" = "file://${./homepage/index.html}";
         };
+        nixExtensions = [
+          (fetchFirefoxAddon{
+            name = "Adblock Plus";
+            url = "https://addons.mozilla.org/firefox/downloads/file/3995494/adblock_plus-3.14.2.xpi";
+          })
+          (fetchFirefoxAddon{
+            name = "AdBlock for Firefox";
+            url = "https://addons.mozilla.org/firefox/downloads/file/4014431/adblock_for_firefox-5.2.0.xpi";
+          })
+          (fetchFirefoxAddon{
+            name = "AdGuard AdBlocker";
+            url = "https://addons.mozilla.org/firefox/downloads/file/4012057/adguard_adblocker-4.1.37.xpi";
+          })
+          (fetchFirefoxAddon{
+            name = "Content Farm Terminator";
+            url = "https://addons.mozilla.org/firefox/downloads/file/4017713/content_farm_terminator-5.9.2.xpi";
+          })
+          (fetchFirefoxAddon{
+            name = "Custom New Tab Page";
+            url = "https://addons.mozilla.org/firefox/downloads/file/3669474/custom_new_tab_page-1.0.0.xpi";
+          })
+          (fetchFirefoxAddon{
+            name = "Enhancer for YouTube";
+            url = "https://addons.mozilla.org/firefox/downloads/file/3964540/enhancer_for_youtube-2.0.115.xpi";
+          })
+          (fetchFirefoxAddon{
+            name = "ImageAssistant Batch Image Downloader";
+            url = "https://addons.mozilla.org/firefox/downloads/file/3976583/ia_batch_image_downloader-1.66.6.xpi";
+          })
+          (fetchFirefoxAddon{
+            name = "Octotree";
+            url = "https://addons.mozilla.org/firefox/downloads/file/3999843/octotree-7.6.1.xpi";
+          })
+          (fetchFirefoxAddon{
+            name = "Saladict";
+            url = "https://addons.mozilla.org/firefox/downloads/file/3854859/ext_saladict-7.20.0.xpi";
+          })
+          (fetchFirefoxAddon{
+            name = "Stylus";
+            url = "https://addons.mozilla.org/firefox/downloads/file/3995806/styl_us-1.5.26.xpi";
+          })
+          (fetchFirefoxAddon{
+            name = "FireShot";
+            url = "https://addons.mozilla.org/firefox/downloads/file/4002435/fireshot-1.11.18.xpi";
+          })
+          (fetchFirefoxAddon{
+            name = "Tampermonkey";
+            url = "https://addons.mozilla.org/firefox/downloads/file/4009746/tampermonkey-4.18.0.xpi";
+          })
+          (fetchFirefoxAddon{
+            name = "Tree Style Tab";
+            url = "https://addons.mozilla.org/firefox/downloads/file/4017073/tree_style_tab-3.9.7.xpi";
+          })
+          (fetchFirefoxAddon{
+            name = "TST Indent Line";
+            url = "https://addons.mozilla.org/firefox/downloads/file/4003179/tst_indent_line-1.2.1.xpi";
+          })
+          (fetchFirefoxAddon{
+            name = "TST Mouse Wheel and Double Click";
+            url = "https://addons.mozilla.org/firefox/downloads/file/3473925/tree_style_tab_mouse_wheel-1.5.xpi";
+          })
+          (fetchFirefoxAddon{
+            name = "TWP";
+            url = "https://addons.mozilla.org/firefox/downloads/file/4011167/traduzir_paginas_web-9.6.1.xpi";
+          })
+          (fetchFirefoxAddon{
+            name = "uBlacklist";
+            url = "https://addons.mozilla.org/firefox/downloads/file/4003811/ublacklist-8.0.2.xpi";
+          })
+          (fetchFirefoxAddon{
+            name = "uBlock Origin";
+            url = "https://addons.mozilla.org/firefox/downloads/file/4003969/ublock_origin-1.44.4.xpi";
+          })
+          (fetchFirefoxAddon{
+            name = "User-Agent Switcher and Manager";
+            url = "https://addons.mozilla.org/firefox/downloads/file/3952467/user_agent_string_switcher-0.4.8.xpi";
+          })
+          (fetchFirefoxAddon{
+            name = "Vim Vixen";
+            url = "https://addons.mozilla.org/firefox/downloads/file/3845233/vim_vixen-1.2.3.xpi";
+          })
+        ];
       };
     };
     profiles.default = {
---
+++ modules/programs/firefox/homepage/config.js
@@ -0,0 +1,230 @@
+// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
+// ╠╩╗║╣ ║║║ ║ ║ ║
+// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
+// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
+// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
+// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+
+const CONFIG = {
+	// ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+	// ├┴┐├─┤└─┐││  └─┐
+	// └─┘┴ ┴└─┘┴└─┘└─┘
+
+	// General
+	name: 'Ruixi-rebirth',
+	imageBackground: false,
+	openInNewTab: true,
+	twelveHourFormat: false,
+
+	// Greetings
+	greetingMorning: 'Good morning!',
+	greetingAfternoon: 'Good afternoon,',
+	greetingEvening: 'Good evening,',
+	greetingNight: 'Go to Sleep!',
+
+	// Layout
+	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+
+	// Weather
+	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherUnit: 'C', // 'F', 'C'
+	language: 'en', // More languages in https://openweathermap.org/current#multi
+
+	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
+	defaultLatitude: '37.775',
+	defaultLongitude: '-122.419',
+
+	// Autochange
+	autoChangeTheme: true,
+
+	// Autochange by OS
+	changeThemeByOS: true,
+
+	// Autochange by hour options (24hrs format, string must be in: hh:mm)
+	changeThemeByHour: false,
+	hourDarkThemeActive: '18:30',
+	hourDarkThemeInactive: '07:00',
+
+	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+	// ├┴┐│ │ │  │ │ ││││└─┐
+	// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+	firstButtonsContainer: [
+		{
+			id: '1',
+			name: 'Github',
+			icon: 'github',
+			link: 'https://github.com/',
+		},
+		{
+			id: '2',
+			name: 'Mail',
+			icon: 'mail',
+			link: 'https://mail.google.com/',
+		},
+		{
+			id: '3',
+			name: 'bilibili',
+			icon: 'play',
+			link: 'https://www.bilibili.com',
+		},
+		{
+			id: '4',
+			name: 'element',
+			icon: 'piggy-bank',
+			link: 'https://app.element.io',
+		},
+		{
+			id: '5',
+			name: 'Reddit',
+			icon: 'glasses',
+			link: 'https://reddit.com',
+		},
+		{
+			id: '6',
+			name: 'youtube',
+			icon: 'youtube',
+			link: 'https://youtube.com',
+		},
+	],
+
+	secondButtonsContainer: [
+		{
+			id: '1',
+			name: 'Music',
+			icon: 'headphones',
+			link: 'https://open.spotify.com',
+		},
+		{
+			id: '2',
+			name: 'twitter',
+			icon: 'twitter',
+			link: 'https://twitter.com/',
+		},
+		{
+			id: '3',
+			name: 'bot',
+			icon: 'bot',
+			link: 'https://discord.com/app',
+		},
+		{
+			id: '4',
+			name: 'Amazon',
+			icon: 'shopping-bag',
+			link: 'https://amazon.com/',
+		},
+		{
+			id: '5',
+			name: 'Hashnode',
+			icon: 'pen-tool',
+			link: 'https://hashnode.com/',
+		},
+		{
+			id: '6',
+			name: 'Figma',
+			icon: 'figma',
+			link: 'https://figma.com/',
+		},
+	],
+
+	// ┬  ┬┌─┐┌┬┐┌─┐
+	// │  │└─┐ │ └─┐
+	// ┴─┘┴└─┘ ┴ └─┘
+
+	// First Links Container
+	firstlistsContainer: [
+		{
+			icon: 'wrench',
+			id: '1',
+			links: [
+				{
+					name: '站长工具',
+					link: 'https://ping.chinaz.com/ruixi.org',
+				},
+				{
+					name: '阿里云盘',
+					link: 'https://www.aliyundrive.com/drive',
+				},
+				{
+					name: '知乎',
+					link: 'https://www.zhihu.com/explore',
+				},
+				{
+					name: 'Deepl',
+					link: 'https://www.deepl.com/zh/translator',
+				},
+			],
+		},
+		{
+			icon: 'coffee',
+			id: '2',
+			links: [
+				{
+					name: 'Roadmap',
+					link: 'https://roadmap.sh/',
+				},
+				{
+					name: 'MDN',
+					link: 'https://developer.mozilla.org/zh-CN/',
+				},
+				{
+					name: 'Stackoverflow',
+					link: 'https://stackoverflow.com/',
+				},
+				{
+					name: 'The way to go',
+					link: 'https://github.com/unknwon/the-way-to-go_ZH_CN/blob/master/eBook/directory.md',
+				},
+			],
+		},
+	],
+
+	// Second Links Container
+	secondListsContainer: [
+		{
+			icon: 'binary',
+			id: '1',
+			links: [
+				{
+					name: 'Spotify',
+					link: 'https://www.spotify.com',
+				},
+				{
+					name: 'Reddit',
+					link: 'https://www.reddit.com',
+				},
+				{
+					name: 'Hashnode',
+					link: 'https://www.hashnode.com',
+				},
+				{
+					name: 'Pocket',
+					link: 'https://www.pocket.com',
+				},
+			],
+		},
+		{
+			icon: 'github',
+			id: '2',
+			links: [
+				{
+					name: 'Front',
+					link: 'https://www.reddit.com/r/Frontend/',
+				},
+				{
+					name: 'Rust',
+					link: 'https://www.reddit.com/r/rust/',
+				},
+				{
+					name: 'Go',
+					link: 'https://www.reddit.com/r/golang/',
+				},
+				{
+					name: 'Repos',
+					link: 'https://github.com/Ruixi-rebirth',
+				},
+			],
+		},
+	],
+};
