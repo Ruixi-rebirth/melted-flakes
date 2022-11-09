b9495be2a4fdcd0f93c8bf89e39ddcbe16cfaf13
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-11-09 17:56:08 +0800

    添加impermanence: 模块

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,4 +1,4 @@
-{ lib, inputs, nixpkgs, home-manager, nur, user, hyprland, ... }:
+{ lib, inputs, nixpkgs, home-manager, nur, user, hyprland, impermanence, ... }:
 
 let
   system = "x86_64-linux"; # System architecture
