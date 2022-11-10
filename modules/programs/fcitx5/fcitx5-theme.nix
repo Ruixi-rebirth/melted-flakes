d49d556a65ddac5717593299fd09e3199bd05520
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-11-10 15:49:52 +0800

    添加fcitx5配置

--- modules/programs/fcitx5/fcitx5-theme.nix
+++ modules/programs/fcitx5/fcitx5-theme.nix
@@ -1,5 +1,7 @@
 { config, lib, pkgs, ... }:
 {
   home.file.".config/fcitx5/conf/classicui.conf".source = ./classicui.conf;
+  home.file.".config/fcitx5/config".source = ./config;
+  home.file.".config/fcitx5/profile".source = ./profile;
   home.file.".local/share/fcitx5/themes/Catppuccin/theme.conf".text = import ./theme.nix;
 }
