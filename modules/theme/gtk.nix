818c2700594382d5b44569949f6920d4343fbecf
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-11-25 16:56:40 +0800

    调节gtk主题

--- modules/theme/gtk.nix
+++ modules/theme/gtk.nix
@@ -37,5 +37,11 @@
       gtk-xft-hintstyle = "hintslight";
       gtk-xft-rgba = "rgb";
     };
+    gtk2.extraConfig = ''
+      gtk-xft-antialias=1
+      gtk-xft-hinting=1
+      gtk-xft-hintstyle="hintslight"
+      gtk-xft-rgba="rgb"
+    '';
   };
 }
