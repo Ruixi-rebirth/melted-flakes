fdd0ba09df90c5e383e6e69a66a35e211163476b
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-11-25 16:50:54 +0800

    移除gtk2主题设置

--- modules/theme/gtk.nix
+++ modules/theme/gtk.nix
@@ -37,11 +37,5 @@
       gtk-xft-hintstyle = "hintslight";
       gtk-xft-rgba = "rgb";
     };
-    gtk2.extraConfig = ''
-      gtk-xft-antialias=1
-      gtk-xft-hinting=1
-      gtk-xft-hintstyle="hintslight"
-      gtk-xft-rgba="rgb"
-    '';
   };
 }
