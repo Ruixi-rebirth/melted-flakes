8b8334c0ba9f90566915b5196c6d21373a19c2cd
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-12-15 10:42:44 +0800

    调整代码结构

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -12,6 +12,7 @@
     (import ../../modules/virtualisation);
 
   users.mutableUsers = false;
+  users.users.root.initialHashedPassword = "$6$4lwj3AGq8M9CQE2.$q8cNPghWHTl/dfE0dMPm2vsh0cMpY2gWxw91/Uadi8jShbvUHJJu3Jg0CvSpqrlEB7a3kvWDf/p2CI3mSqP1c/";
   users.users.${user} = {
     initialHashedPassword = "$6$4lwj3AGq8M9CQE2.$q8cNPghWHTl/dfE0dMPm2vsh0cMpY2gWxw91/Uadi8jShbvUHJJu3Jg0CvSpqrlEB7a3kvWDf/p2CI3mSqP1c/";
     shell = pkgs.fish;
--- hosts/system.nix
+++ hosts/system.nix
@@ -23,15 +23,6 @@
       enable = true;
     };
   };
-  users.users.root.initialHashedPassword = "$6$4lwj3AGq8M9CQE2.$q8cNPghWHTl/dfE0dMPm2vsh0cMpY2gWxw91/Uadi8jShbvUHJJu3Jg0CvSpqrlEB7a3kvWDf/p2CI3mSqP1c/";
-  users.users.${user} = {
-    initialHashedPassword = "$6$4lwj3AGq8M9CQE2.$q8cNPghWHTl/dfE0dMPm2vsh0cMpY2gWxw91/Uadi8jShbvUHJJu3Jg0CvSpqrlEB7a3kvWDf/p2CI3mSqP1c/";
-    shell = pkgs.fish;
-    isNormalUser = true;
-    extraGroups = [ "wheel" "docker" ];
-    packages = with pkgs; [
-    ];
-  };
   environment = {
     binsh = "${pkgs.dash}/bin/dash";
     shells = with pkgs; [ fish ];
