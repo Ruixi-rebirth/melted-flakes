d61de5f2984c57c5546fc62cc92b5c703c67611b
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-12-17 11:57:45 +0800

    更新qemu配置

--- modules/virtualisation/qemu.nix
+++ modules/virtualisation/qemu.nix
@@ -6,7 +6,12 @@
       virt-manager
     ];
   };
-  virtualisation.libvirtd.enable = true;
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      onBoot = "ignore";
+    };
+  };
   networking.firewall.trustedInterfaces = [ "virbr0" ];
   programs.dconf.enable = true;
   users.groups.libvirtd.members = [ "${user}" ];
