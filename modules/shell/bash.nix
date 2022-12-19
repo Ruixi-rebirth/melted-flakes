3f6f2b895ea72e5d60971bbf3dd8e7f58913fdd5
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-12-19 19:19:53 +0800

    更换login shell: fish->bash

--- modules/shell/bash.nix
+++ modules/shell/bash.nix
@@ -3,14 +3,22 @@
   programs = {
     bash = {
       enable = true;
+      loginShellInit = ''
+        # if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
+        #    exec dbus-run-session sway --unsupported-gpu
+        #  fi
+        if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
+           exec dbus-run-session Hyprland
+         fi
+      '';
     };
-    initExtra = ''
-      # if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
-      #    exec dbus-run-session sway --unsupported-gpu
-      #  fi
-      if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
-         exec dbus-run-session Hyprland
-       fi
-    '';
+    # initExtra = ''
+    #   # if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
+    #   #    exec dbus-run-session sway --unsupported-gpu
+    #   #  fi
+    #   if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
+    #      exec dbus-run-session Hyprland
+    #    fi
+    # '';
   };
 }
