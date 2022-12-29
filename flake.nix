1b5a4facef0270167ac8e51243381449d05b894a
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-12-30 05:13:34 +0800

    调整代码结构

--- flake.nix
+++ flake.nix
@@ -6,15 +6,11 @@
       nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
       neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
       rust-overlay.url = "github:oxalica/rust-overlay";
+      impermanence.url = "github:nix-community/impermanence";
+      nur.url = "github:nix-community/NUR";
       home-manager = {
         url = "github:nix-community/home-manager";
         inputs.nixpkgs.follows = "nixpkgs";
-      };
-      impermanence = {
-        url = "github:nix-community/impermanence";
-      };
-      nur = {
-        url = "github:nix-community/NUR";
       };
       hyprland = {
         url = "github:hyprwm/Hyprland";
@@ -32,7 +28,7 @@
         import ./hosts {
           # Imports ./hosts/default.nix
           inherit (nixpkgs) lib;
-          inherit inputs nixpkgs home-manager nur user hyprland impermanence rust-overlay; # Also inherit home-manager so it does not need to be defined here.
+          inherit inputs nixpkgs home-manager nur user hyprland impermanence rust-overlay;
         }
       );
     };
