58301481a4fdd526810b53ddc7ab6fc48b5d6a37
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-12-30 08:38:57 +0800

    更新neovim配置: 与rust有关的东西

--- modules/editors/nvim/lua/plugins/configs/dap.lua
+++ modules/editors/nvim/lua/plugins/configs/dap.lua
@@ -1,7 +1,7 @@
 local dap = require("dap")
 dap.adapters.lldb = {
 	type = "executable",
-	command = "lldb", -- adjust as needed, must be absolute path
+	command = "/etc/profiles/per-user/ruixi/bin/lldb", -- adjust as needed, must be absolute path
 	name = "lldb",
 }
 dap.configurations.cpp = {
