06956d422eaa0858c99b74e1f3029ff9c6fb0c49
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-01-04 23:57:47 +0800

    更新

--- pkgs/qq/default.nix
+++ pkgs/qq/default.nix
@@ -1,88 +1,66 @@
-{ alsa-lib
-, cups
-, dpkg
-, fetchurl
-, gjs
-, glib
-, gtk3
-, lib
-, libdrm
-, libgcrypt
-, libkrb5
-, mesa # for libgbm
-, nss
-, xorg
-, systemd
-, stdenv
-, at-spi2-core
-, autoPatchelfHook
-, wrapGAppsHook
-, copyDesktopItems
-, makeDesktopItem
-}:
+{ appimageTools, lib, fetchurl, fetchzip, electron, makeWrapper, libsecret, p7zip }:
 
 let
-  version = "3.0.0-571";
+  pname = "qq";
+  version = "3.0.0-565";
+  name = "Tencent-QQ-${version}-bwrap";
+
   srcs = {
-    x86_64-linux = fetchurl {
-      url = "https://dldir1.qq.com/qqfile/qq/QQNT/c005c911/linuxqq_${version}_amd64.deb";
-      sha256 = "sha256-8KcUhZwgeFzGyrQITWnJUzEPGZOCj0LIHLmRuKqkgmQ=";
-    };
-    aarch64-linux = fetchurl {
-      url = "https://dldir1.qq.com/qqfile/qq/QQNT/c005c911/linuxqq_${version}_arm64.deb";
-      sha256 = "";
+    electron = fetchurl {
+      url = "https://dldir1.qq.com/qqfile/qq/QQNT/64bd2578/linuxqq_3.0.0-565_x86_64.AppImage";
+      sha256 = "sha256-uoxGwuG+RP1XiNEvQZ5yLHh2rYU1TxBsl9NYaYZ78AI=";
     };
   };
-  src = srcs.${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
+
+  src = srcs.electron;
+
+  appimageContents = (appimageTools.extract { inherit name src; });
+
 in
-stdenv.mkDerivation {
-  pname = "qq";
-  inherit version src;
+appimageTools.wrapAppImage {
+  inherit version name;
+  src = appimageContents;
 
-  unpackCmd = "dpkg-deb -x $curSrc source";
-
-  nativeBuildInputs = [
-    autoPatchelfHook
-    wrapGAppsHook
-    copyDesktopItems
-    dpkg
-  ];
-
-  buildInputs = [
-    alsa-lib
-    at-spi2-core
-    cups
-    libdrm
-    libgcrypt
-    libkrb5
-    mesa
-    nss
-    xorg.libXdamage
-  ];
-
-  runtimeDependencies = [
-    gtk3
-    (lib.getLib systemd)
-  ];
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin
-    cp -r opt $out/opt
-    cp -r usr/share $out/share
-    substituteInPlace $out/share/applications/qq.desktop \
-      --replace "/opt" "$out/opt" \
-      --replace "/usr/share" "$out/share"
-    ln -s $out/opt/QQ/qq $out/bin/qq
-    runHook postInstall
+  extraInstallCommands = ''
+    mv $out/bin/${name} $out/bin/${pname}
+    install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
+    substituteInPlace $out/share/applications/${pname}.desktop \
+      --replace 'Exec=AppRun' 'Exec=${pname}' \
+      --replace 'Icon=/opt/QQ/resources/app/512x512.png' 'Icon=qq'
+    cp -r ${appimageContents}/usr/share/icons $out/share
+    cp $out/bin/${pname} $out/bin/test
+    mv $out/bin/test $out/bin/${pname}
+    substituteInPlace $out/bin/${pname} \
+      --replace "auto_mounts[@]" 'no_automounts[@]'
+    sed -i '102 i \ \ --ro-bind /sys /sys' $out/bin/${pname}
+    sed -i '102 i \ \ --dev-bind /run/dbus /run/dbus' $out/bin/${pname}
+    sed -i '102 i \ \ --bind /run/user/$(id -u) /run/user/$(id -u)' $out/bin/${pname}
+    sed -i '102 i \ \ --ro-bind-try /etc/fonts /etc/fonts' $out/bin/${pname}
+    sed -i '102 i \ \ --dev-bind /tmp /tmp' $out/bin/${pname}
+    sed -i '102 i \ \ --bind "\$HOME/.pki" "\$HOME/.pki"' $out/bin/${pname}
+    sed -i '102 i \ \ --ro-bind "\$HOME/.Xauthority" "\$HOME/.Xauthority"' $out/bin/${pname}
+    sed -i '102 i \ \ --bind "$(xdg-user-dir)/.config/QQ" "$(xdg-user-dir)/.config/QQ"' $out/bin/${pname}
+    sed -i '102 i \ \ --bind "$(xdg-user-dir DOWNLOAD)" "$(xdg-user-dir DOWNLOAD)"' $out/bin/${pname}
+    sed -i '102 i \ \ --bind "/run/opengl-driver/" "/run/opengl-driver/"' $out/bin/${pname}
+    sed -i '102 i \ \ --setenv IBUS_USE_PORTAL 1' $out/bin/${pname}
+    sed -i '102 i \ \ --setenv GTK_IM_MODULE $GTK_IM_MODULE' $out/bin/${pname}
+    sed -i '102 i \ \ --setenv DISPLAY $\{DISPLAY}' $out/bin/${pname}
+    sed -i '102 i \ \ --bind /run/current-system/sw /run/current-system/sw' $out/bin/${pname}
+    sed -i '102 i \ \ --ro-bind "\$HOME/.icons" "\$HOME/.icons"' $out/bin/${pname}
+    sed -i '102 i \ \ --ro-bind "$(xdg-user-dir)/.local/share/fcitx5" "$(xdg-user-dir)/.local/share/fcitx5"' $out/bin/${pname}
   '';
 
+  #passthru.version = version;
+
+  extraPkgs = pkgs: with pkgs; [
+    libsecret
+    libappindicator-gtk3
+  ];
+
   meta = with lib; {
-    homepage = "https://im.qq.com/linuxqq/";
-    description = "Messaging app";
-    platforms = [ "x86_64-linux" "aarch64-linux" ];
+    homepage = "https://im.qq.com";
+    description = "Official Tencent QQ client for Linux (Beta) with enhanced bwrap rules";
+    platforms = [ "x86_64-linux" ];
     license = licenses.unfree;
-    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
-    maintainers = with lib.maintainers; [ fee1-dead ];
   };
 }
