6891ab50cd5d8b1121a9e4a6e4da46fbd8f321e3
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-01-06 02:48:29 +0800

    添加包: hyprpicker grimblast

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -114,9 +114,6 @@
       ntfs3g
       pkgs.rust-bin.stable.latest.default
       blender
-      inputs.hypr-contrib.packages.${pkgs.system}.grimblast
-      inputs.hyprpicker.packages.${pkgs.system}.hyprpicker
-
     ];
   };
 
--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -1,7 +1,11 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 {
   imports = [ ../../programs/waybar/hyprland_waybar.nix ];
 
+  environment.systemPackages = with pkgs; [
+    inputs.hypr-contrib.packages.${pkgs.system}.grimblast
+    inputs.hyprpicker.packages.${pkgs.system}.hyprpicker
+  ];
   programs.hyprland = {
     enable = true;
   };
