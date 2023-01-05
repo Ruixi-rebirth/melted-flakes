fe85314a70c0eaacfb1711ae10c5c1257d45bd0e
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-01-06 01:56:16 +0800

    更新

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,7 +3,7 @@
 {
   catppuccin-cursors = pkgs.callPackage ./catppuccin-cursors { };
   catppuccin-gtk = pkgs.callPackage ./catppuccin-gtk { };
-  # qq = pkgs.callPackage ./qq { };
+  qq = pkgs.callPackage ./qq { };
   # netease-cloud-music-gtk = pkgs.callPackage ./netease-cloud-music-gtk { };
   hyprpicker = pkgs.callPackage ./hyprpicker { };
 }
