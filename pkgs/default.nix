382b626f689ed4d94362feb0e1f935d3eda0a04c
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-01-06 02:50:48 +0800

    添加包: hyprpicker grimblast

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,5 +5,4 @@
   catppuccin-gtk = pkgs.callPackage ./catppuccin-gtk { };
   qq = pkgs.callPackage ./qq { };
   # netease-cloud-music-gtk = pkgs.callPackage ./netease-cloud-music-gtk { };
-  # hyprpicker = pkgs.callPackage ./hyprpicker { };
 }
