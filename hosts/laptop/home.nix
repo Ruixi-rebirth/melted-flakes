1ae06f28b9856970b50fb7fd9deef9c9f8c38434
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-01-11 12:30:30 +0800

    更新

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -1,15 +1,16 @@
 { config, lib, pkgs, user, impermanence, ... }:
 
 {
-  imports =
-    # [ (import ../../modules/desktop/sway/home.nix) ] ++
-    [ (import ../../modules/desktop/hyprland/home.nix) ] ++
-    [ (import ../../modules/scripts/home.nix) ] ++
-    (import ../../modules/shell) ++
-    (import ../../modules/editors) ++
-    (import ../../modules/programs) ++
-    (import ../../modules/theme/catppuccin-frappe-dark) ++
-    (import ../../modules/devlop);
+  imports = [
+    ../../modules/desktop/sway
+    ../../modules/desktop/hyprland
+    ../../modules/scripts
+  ] ++
+  (import ../../modules/shell) ++
+  (import ../../modules/editors) ++
+  (import ../../modules/programs) ++
+  (import ../../modules/theme/catppuccin-frappe-dark) ++
+  (import ../../modules/devlop);
 
   home = {
     username = "ruixi";
