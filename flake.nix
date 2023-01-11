e56808b13bf9f0ebae12ed562fa1e697f9de77b4
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-01-12 01:10:26 +0800

    更新flake

--- flake.nix
+++ flake.nix
@@ -4,7 +4,10 @@
   inputs =
     {
       nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-      neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+      neovim-nightly-overlay = {
+        url = "github:nix-community/neovim-nightly-overlay";
+        inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=fad51abd42ca17a60fc1d4cb9382e2d79ae31836";
+      };
       rust-overlay.url = "github:oxalica/rust-overlay";
       impermanence.url = "github:nix-community/impermanence";
       nur.url = "github:nix-community/NUR";
