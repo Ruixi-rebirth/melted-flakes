1305b0b18c3d1cae7437487828e0f04bbc3bc113
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-01-13 01:43:14 +0800

    更新

--- modules/theme/catppuccin-light/neovim/default.nix
+++ modules/theme/catppuccin-light/neovim/default.nix
@@ -1,5 +1,7 @@
 { config, lib, pkgs, ... }:
 {
   home.file.".config/nvim/init.lua".source = ./nvim/init.lua;
-  home.file.".config/nvim/lua".source = ./nvim/lua;
+  home.file.".config/nvim/lua/ui".source = ./nvim/lua/ui;
+  home.file.".config/nvim/lua/core".source = ./nvim/lua/core;
+  home.file.".config/nvim/lua/plugins".source = ./nvim/lua/plugins;
 }
