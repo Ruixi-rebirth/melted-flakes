6649b6dafb07a62bee186ee4beaa92ce55c5a6da
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-01-14 17:25:32 +0800

    调整waybar overlay

--- modules/programs/waybar/hyprland_waybar.nix
+++ modules/programs/waybar/hyprland_waybar.nix
@@ -12,8 +12,7 @@ in
     (final: prev: {
       waybar =
         let
-          hyprctl = "${pkgs.hyprland}/bin/hyprctl";
-          waybarPatchFile = import ./workspace-patch.nix { inherit pkgs hyprctl; };
+          waybarPatchFile = import ./workspace-patch.nix;
         in
         prev.waybar.overrideAttrs (oldAttrs: {
           mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
