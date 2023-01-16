bcd1d229318f883e6794e0d49d77c5f7f568dfdf
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-01-16 21:52:42 +0800

    调整主题: nord

--- modules/theme/nord/fish/default.nix
+++ modules/theme/nord/fish/default.nix
@@ -1,5 +1,5 @@
 { lib, pkgs, user, ... }:
 
 {
-  home.file.".config/fish/conf.d/latte.fish".text = import ./conf.d/latte_theme.nix;
+  home.file.".config/fish/conf.d/nord.fish".text = import ./conf.d/nord_theme.nix;
 }
