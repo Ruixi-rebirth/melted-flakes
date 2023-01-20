6d80e96bd32fbc172a858b0cf74986b0735b9cc7
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-01-21 01:02:30 +0800

    更新

--- modules/shell/fish/functions/xdg-set.nix
+++ modules/shell/fish/functions/xdg-set.nix
@@ -7,11 +7,17 @@
         set -a arr2 $arr1
       end
     end 
+    set new1 (echo $arr2 | sed 's/^[ \t]*//g')
+    for new2 in $new1
+      if test -d $new2
+        set -a new3 $new1
+      end
+   end
     set filename (f)
     if test "$filename" = ""
     else 
       set beforeprogram (xdg-mime query default (xdg-mime query filetype $filename))
-      set program (find $arr2 -type l | f) 
+      set program (find $new3 -type l | f) 
       if test "$program" = ""
       else
         echo "Open with $beforeprogram before changing"
