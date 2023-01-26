662ae485396446686141dcaae2a3b73b52b0817d
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-01-26 10:57:01 +0800

    更新neovim配置

--- modules/editors/nvim/lua/plugins/lsp.lua
+++ modules/editors/nvim/lua/plugins/lsp.lua
@@ -127,6 +127,7 @@ return {
 					workspace = {
 						-- Make the server aware of Neovim runtime files
 						library = vim.api.nvim_get_runtime_file("", true),
+						checkThirdParty = false,
 					},
 					-- Do not send telemetry data containing a randomized but unique identifier
 					telemetry = {
