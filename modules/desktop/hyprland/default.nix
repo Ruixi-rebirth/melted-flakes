4608589989ae067d9ff92496bd0868c187a34a44
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-02-07 20:41:03 +0800

    调整有关swww的配置

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -1,16 +1,5 @@
 { config, lib, pkgs, inputs, ... }:
-let
-  swwwInit = pkgs.writeShellScript "swwwInit" ''
-     ${pkgs.swww}/bin/swww-daemon
-      if [[ "$GTK_THEME" == "Catppuccin-Frappe-Pink" ]]; then
-      ${pkgs.swww}/bin/swww img "${../../theme/catppuccin-dark/wall/default.png}" --transition-type random
-    elif [[ "$GTK_THEME" == "Catppuccin-Latte-Green" ]]; then
-      ${pkgs.swww}/bin/swww img "${../../theme/catppuccin-light/wall/default.png}" --transition-type random
-    else 
-      ${pkgs.swww}/bin/swww img "${../../theme/nord/wall/default.png}" --transition-type random
-    fi
-  '';
-in
+
 {
   imports = [ ../../programs/waybar/hyprland_waybar.nix ];
 
@@ -27,7 +16,16 @@ in
     wantedBy = [ "graphical-session.target" ];
     serviceConfig = {
       Type = "simple";
-      ExecStart = "${swwwInit}";
+      ExecStart = ''
+          ${pkgs.swww}/bin/swww-daemon
+          if [[ "$GTK_THEME" == "Catppuccin-Frappe-Pink" ]]; then
+          ${pkgs.swww}/bin/swww img "${../../theme/catppuccin-dark/wall/default.png}" --transition-type random
+        elif [[ "$GTK_THEME" == "Catppuccin-Latte-Green" ]]; then
+          ${pkgs.swww}/bin/swww img "${../../theme/catppuccin-light/wall/default.png}" --transition-type random
+        else 
+          ${pkgs.swww}/bin/swww img "${../../theme/nord/wall/default.png}" --transition-type random
+        fi
+      '';
       ExecStop = "${pkgs.swww}/bin/swww kill";
     };
   };
