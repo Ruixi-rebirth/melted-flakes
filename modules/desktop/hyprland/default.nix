8d61cb5c75169b34db1ba8c3c1a5dfc1b15712bf
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-02-07 21:01:56 +0800

    调整有关swww的配置

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -17,7 +17,7 @@
     serviceConfig = {
       Type = "simple";
       ExecStart = ''
-          setsid ${pkgs.swww}/bin/swww-daemon &
+          ${pkgs.util-linux}/bin/setsid ${pkgs.swww}/bin/swww-daemon &
           if [[ "$GTK_THEME" == "Catppuccin-Frappe-Pink" ]]; then
           ${pkgs.swww}/bin/swww img "${../../theme/catppuccin-dark/wall/default.png}" --transition-type random
         elif [[ "$GTK_THEME" == "Catppuccin-Latte-Green" ]]; then
