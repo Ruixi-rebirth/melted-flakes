01a4297e6cb3550ee1db7eeba031c45066a9aa69
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-02-10 02:51:23 +0800

    更新

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,16 +1,12 @@
 final: prev:
-{ } // (
-  let
-    pkgs = prev;
-  in
-  {
-    catppuccin-cursors = pkgs.callPackage ./catppuccin-cursors { };
-    catppuccin-frappe-gtk = pkgs.callPackage ./catppuccin-frappe-gtk { };
-    catppuccin-latte-gtk = pkgs.callPackage ./catppuccin-latte-gtk { };
-    qq = pkgs.callPackage ./qq { };
-    go-musicfox = pkgs.callPackage ./go-musicfox { };
-    swww = pkgs.callPackage ./swww { };
-    fcitx5-pinyin-moegirl = pkgs.callPackage ./fcitx5-pinyin-moegirl { };
-    fcitx5-pinyin-zhwiki = pkgs.callPackage ./fcitx5-pinyin-zhwiki { };
-  }
-)
+let
+  dirContents = builtins.readDir ../pkgs;
+  genPackage = name: {
+    inherit name;
+    value = final.callPackage (../pkgs + "/${name}") { };
+  };
+  names = builtins.attrNames dirContents;
+in
+builtins.listToAttrs (map genPackage names)
+
+  
