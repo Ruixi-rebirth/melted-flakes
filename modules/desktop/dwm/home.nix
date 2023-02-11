42e952887abfbaea5bcbeb595220ed5f33e2a3aa
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-02-12 01:51:16 +0800

    切换到dwm

--- modules/desktop/dwm/home.nix
+++ modules/desktop/dwm/home.nix
@@ -4,15 +4,15 @@
   imports = [
     (import ../../environment/dwm-variables.nix)
   ];
-  # programs = {
-  #   bash = {
-  #     initExtra = ''
-  #       if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
-  #          exec  startx
-  #       fi
-  #     '';
-  #   };
-  # };
+  programs = {
+    bash = {
+      initExtra = ''
+        if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
+           exec  startx
+        fi
+      '';
+    };
+  };
   home.file = {
     ".xinitrc".text = ''
       if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
@@ -22,7 +22,7 @@
       if command -v dbus-update-activation-environment >/dev/null 2>&1; then
        dbus-update-activation-environment DISPLAY XAUTHORITY
       fi
-       QT_QPA_PLATFORMexec dwm
+      exec dwm
     '';
   };
 }
