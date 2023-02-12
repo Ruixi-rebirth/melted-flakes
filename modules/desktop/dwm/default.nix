8ce0a4aebecfa290184a9477ef52359c5346c2ee
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-02-12 11:37:21 +0800

    修复鼠标指针主题不加载

--- modules/desktop/dwm/default.nix
+++ modules/desktop/dwm/default.nix
@@ -4,12 +4,14 @@
   services.xserver = {
     enable = true;
     windowManager.dwm.enable = true;
-    displayManager.startx.enable = true;
-    sessionCommands = ''
-      ${pkgs.xorg.xrdb}/bin/xrdb -merge <${pkgs.writeText "Xresources" ''
-        Xcursor.theme: Catppuccin-Frappe-Dark
-      ''}
-    '';
+    displayManager = {
+      startx.enable = true;
+      sessionCommands = ''
+        ${pkgs.xorg.xrdb}/bin/xrdb -merge <${pkgs.writeText "Xresources" ''
+          Xcursor.theme: Catppuccin-Frappe-Dark
+        ''}
+      '';
+    };
   };
   environment.systemPackages = with pkgs; [
     betterlockscreen
