b35d36fd529eb95573f397513d554ad2629688dc
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-02-13 15:10:47 +0800

    切换到dwm

--- modules/desktop/dwm/default.nix
+++ modules/desktop/dwm/default.nix
@@ -6,6 +6,15 @@
     windowManager.dwm.enable = true;
     displayManager = {
       startx.enable = true;
+      sessionCommands = ''systemctl --user start hyprland-session.target'';
+    };
+  };
+  systemd.user.targets.dwm-session = {
+    Unit = {
+      Description = "dwm session";
+      BindsTo = [ "graphical-session.target" ];
+      Wants = [ "graphical-session-pre.target" ];
+      After = [ "graphical-session-pre.target" ];
     };
   };
   environment.systemPackages = with pkgs; [
