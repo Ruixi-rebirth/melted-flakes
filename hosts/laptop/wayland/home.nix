ac5c3b6e64b4c7309a497fb31c9e09ef9f304399
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-02-21 15:04:25 +0800

    切换到hyprland
    切换到hyprland

--- hosts/laptop/wayland/home.nix
+++ hosts/laptop/wayland/home.nix
@@ -2,15 +2,15 @@
 
 {
   imports =
-    [ (import ../../../modules/desktop/sway/home.nix) ] ++
-    # [ (import ../../../modules/desktop/hyprland/home.nix) ] ++
+    # [ (import ../../../modules/desktop/sway/home.nix) ] ++
+    [ (import ../../../modules/desktop/hyprland/home.nix) ] ++
     [ (import ../../../modules/scripts) ] ++
     (import ../../../modules/shell) ++
     (import ../../../modules/editors) ++
     (import ../../../modules/programs/wayland) ++
-    (import ../../../modules/theme/catppuccin-dark/wayland) ++
+    # (import ../../../modules/theme/catppuccin-dark/wayland) ++
     # (import ../../../modules/theme/catppuccin-light/wayland) ++
-    # (import ../../../modules/theme/nord/wayland) ++
+    (import ../../../modules/theme/nord/wayland) ++
     (import ../../../modules/devlop);
 
   home = {
