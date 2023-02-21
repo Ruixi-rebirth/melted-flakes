7fe53bd1ee1e066377a15880bea5b00f9c43b520
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-02-21 16:18:45 +0800

    test

--- modules/programs/x11/polybar/default.nix
+++ modules/programs/x11/polybar/default.nix
@@ -5,6 +5,7 @@
       enable = true;
       package = pkgs.polybar.override {
         alsaSupport = true;
+        pulseSupport = true;
         mpdSupport = true;
       };
       script = "polybar -q main &";
