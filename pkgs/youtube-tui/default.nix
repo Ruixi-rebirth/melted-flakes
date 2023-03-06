cd30272bd70450857d96b618660e2851ff758ed4
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-03-06 18:34:09 +0800

    test

--- pkgs/youtube-tui/default.nix
+++ pkgs/youtube-tui/default.nix
@@ -1,29 +1,33 @@
 { stdenv
 , lib
-, fetchurl
+, fetchCrate
 , rustPlatform
 , pkg-config
 , libsixel
 , libxcb
 , gcc
+, openssl
+, libressl
 }:
 rustPlatform.buildRustPackage rec {
   pname = "youtube-tui";
   version = "0.7.0";
-  cargoLock = {
-    lockFile = ./Cargo.lock;
+
+  src = fetchCrate {
+    inherit pname version;
+    # url = "https://crates.io/api/v1/crates/youtube-tui/0.7.0/download";
+    sha256 = "sha256-Rz13mGqxfCU+6k2Ejvk+ZoaLirDT6VkmAIhvFdNRvuI=";
   };
 
-  src = fetchurl {
-    url = "https://crates.io/api/v1/crates/youtube-tui/0.7.0/download";
-    sha256 = "sha256-SKKlRu8PpTG7c/E3F4r+K2xR0TB0v/NMHT5IZWIO2ik=";
-  };
-
-  # cargoSha256 = lib.fakeSha256;
-
-  nativeBuildInputs = [ pkg-config ];
-
-  buildInputs = [ gcc libsixel libxcb ];
+  # dontUnpack = true;
+  # unpackPhase = ''
+  #   ${pkgs.p7zip}/bin/7z x $src
+  # '';
+  # cargoSha256 = "";
+  cargoHash = "sha256-qcWuh8qaOQBBebdX3D01k5yXZfifbFC+ZP0d6bJeOr0=";
+  # cargoDepsName = pname;
+  nativeBuildInputs = [ openssl ];
+  buildInputs = [ gcc libsixel libxcb pkg-config ];
 
   doCheck = false;
 
