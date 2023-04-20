d24ab1d7be98032c20e554022e492039a41657c0
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-04-20 15:39:04 +0800

    fix(waybar): waybar workspace patch for Hyprland

--- modules/programs/wayland/waybar/hyprland_waybar.nix
+++ modules/programs/wayland/waybar/hyprland_waybar.nix
@@ -14,10 +14,8 @@
         in
         prev.waybar.overrideAttrs (oldAttrs: {
           mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
-          patches = (oldAttrs.patches or [ ]) ++ [ waybarPatchFile ];
-          # postPatch = (oldAttrs.postPatch or "") + ''
-          #   sed -i 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());/g' src/modules/wlr/workspace_manager.cpp
-          # '';
+          postPatch = (oldAttrs.postPatch or "") + ''
+            sed -i 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());/g' src/modules/wlr/workspace_manager.cpp'';
         });
     })
   ];
