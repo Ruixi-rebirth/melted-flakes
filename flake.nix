a7d13a439d0c22be8a597197a289ae98b2bb053a
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-04-29 09:55:38 +0800

    refactor[devShells]: by using mission-control

--- flake.nix
+++ flake.nix
@@ -9,7 +9,11 @@
     in
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
-      perSystem = { config, inputs', pkgs, system, ... }:
+      imports = [
+        inputs.flake-root.flakeModule
+        inputs.mission-control.flakeModule
+      ];
+      perSystem = { config, inputs', pkgs, system, lib, ... }:
         let
           pkgs = import nixpkgs {
             inherit system;
@@ -19,22 +23,38 @@
           };
         in
         {
+          mission-control.scripts = {
+            fmt = {
+              description = "Format the top-level Nix files(nixpkgs-fmt)";
+              exec = "${lib.getExe pkgs.nixpkgs-fmt} ./*.nix";
+              category = "Tools";
+            };
+            blog = {
+              description = "debug my blog";
+              exec = ''
+                cd "$FLAKE_ROOT/blog"
+                cp -r ./static/hugo-theme-stack ./themes/
+                ${lib.getExe pkgs.hugo} server --buildDrafts --forceSyncStatic'';
+              category = "Tools";
+            };
+            disko = {
+              description = "Pre-install,automatically partition and moount ";
+              exec = "nix --extra-experimental-features nix-command --extra-experimental-features flakes run github:nix-community/disko -- --mode zap_create_mount /path/to/disko-config.nix";
+              category = "Tools";
+            };
+          };
+
           devShells = {
             #run by `nix devlop` or `nix-shell`(legacy)
-            default = import ./shell.nix { inherit pkgs; };
-            #run by `nix devlop .#<name>`
-            blog = with pkgs; mkShell {
-              name = "blog";
-              nativeBuildInputs = [
-                hugo
+            # default = import ./shell.nix { inherit pkgs; };
+            default = pkgs.mkShell {
+              nativeBuildInputs = with pkgs; [ git neovim colmena ];
+              inputsFrom = [
+                config.flake-root.devShell
+                config.mission-control.devShell
               ];
-              shellHook = ''
-                export PS1="\e[0;32m(Blog)\$ \e[m" 
-                cd ./blog
-                cp -r ./static/hugo-theme-stack ./themes/
-                #hugo server --buildDrafts --forceSyncStatic
-              '';
             };
+            #run ny `nix devShell .#<name>`
             secret = with pkgs; mkShell {
               name = "secret";
               nativeBuildInputs = [
@@ -48,8 +68,6 @@
               '';
             };
           };
-
-          formatter = pkgs.nixpkgs-fmt;
         };
 
       flake = {
@@ -83,6 +101,8 @@
         url = "github:nix-community/home-manager";
         inputs.nixpkgs.follows = "nixpkgs";
       };
+      flake-root.url = "github:srid/flake-root";
+      mission-control.url = "github:Platonic-Systems/mission-control";
       emacs-overlay.url = "github:nix-community/emacs-overlay";
       # lanzaboote = {
       #   url = "github:nix-community/lanzaboote";
