f6b60f8b5dae2cd15c2f20480d35eb635943dddb
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-04-29 22:39:42 +0800

    feat(disko): add a simple disk-luks layout sample

---
+++ hosts/laptop/disko-luks.nix
@@ -0,0 +1,74 @@
+{ disks ? [ "/dev/vdb" ], ... }: {
+  disko.devices = {
+    disk = {
+      vdb = {
+        type = "disk";
+        device = builtins.elemAt disks 0;
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [
+            {
+              name = "ESP";
+              start = "1MiB";
+              end = "100MiB";
+              bootable = true;
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "defaults"
+                ];
+              };
+            }
+            {
+              name = "luks";
+              start = "100MiB";
+              end = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                extraOpenArgs = [ "--allow-discards" ];
+                keyFile = "/tmp/secret.key";
+                content = {
+                  type = "lvm_pv";
+                  vg = "pool";
+                };
+              };
+            }
+          ];
+        };
+      };
+    };
+    lvm_vg = {
+      pool = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100M";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              mountOptions = [
+                "defaults"
+              ];
+            };
+          };
+          home = {
+            size = "10M";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/home";
+            };
+          };
+          raw = {
+            size = "10M";
+          };
+        };
+      };
+    };
+  };
+}
